81adea67a177a72759980858a94c65ea440263f0
Apizyy <muhamadhapis709@gmail.com>
2026-01-14 22:58:03 +0700

    logout.php

--- logout.php
+++ logout.php
@@ -1,3 +1,23 @@
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle text-danger fw-bold"
+       href="#" role="button" data-bs-toggle="dropdown">
+        <?= $_SESSION['username']; ?>
+    </a>
+    <ul class="dropdown-menu dropdown-menu-end">
+        <li>
+            <a class="dropdown-item" href="admin.php?page=profile">
+                Profile
+            </a>
+        </li>
+        <li><hr class="dropdown-divider"></li>
+        <li>
+            <a class="dropdown-item text-danger" href="logout.php">
+                Logout
+            </a>
+        </li>
+    </ul>
+</li>
+
 <?php
 session_start();
 session_destroy();
